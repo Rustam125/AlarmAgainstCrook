5251577df4f69a1ec0df2f9eae7ee5a1843068c1
Гараев Рустам <r.garaev@reo.ru>
2024-11-16 22:24:38 +0400

    Добавляет запуск сигнализации при входе

--- AlarmAgainstCrook/Assets/Scripts/Alarm.cs
+++ AlarmAgainstCrook/Assets/Scripts/Alarm.cs
@@ -1,26 +1,84 @@
+using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class Alarm : MonoBehaviour
 {
-    [SerializeField] private Door _door;
     [SerializeField] private AudioSource _audioSource;
 
+    private const float MinVolume = 0f;
+    private const float MaxVolume = 1f;
+    private const float Duration = 0.5f;
+
     private bool _isSignalized;
-    
+    private Coroutine _coroutine;
+
     private void Awake()
     {
         _isSignalized = false;
-        _door.Opened += Signalize;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Player>() == false)
+        {
+            return;
+        }
+
+        _isSignalized = !_isSignalized;
+        Signalize();
+    }
+
+    private void OnDestroy()
+    {
+        StopSignalCoroutine();
     }
 
     private void Signalize()
     {
+        StopSignalCoroutine();
+
         if (_isSignalized)
         {
-            return;
+            _audioSource.volume = MinVolume;
+            _audioSource.Play();
+            _coroutine = StartCoroutine(SmoothSignalIncrease());
         }
-        
-        _audioSource.Play();
-        _isSignalized = true;
+        else
+        {
+            _coroutine = StartCoroutine(SmoothSignalReduce());
+        }
+    }
+
+    private IEnumerator SmoothSignalIncrease()
+    {
+        var volume = MinVolume;
+
+        while (volume < MaxVolume)
+        {
+            volume += Time.deltaTime * Duration;
+            _audioSource.volume = volume;
+            yield return null;
+        }
+    }
+
+    private IEnumerator SmoothSignalReduce()
+    {
+        var volume = MaxVolume;
+
+        while (volume > MinVolume)
+        {
+            volume -= Time.deltaTime * Duration;
+            _audioSource.volume = volume;
+            yield return null;
+        }
+    }
+
+    private void StopSignalCoroutine()
+    {
+        if (_coroutine != null)
+        {
+            StopCoroutine(_coroutine);
+        }
     }
 }
--- AlarmAgainstCrook/Assets/Scripts/Door.cs
+++ AlarmAgainstCrook/Assets/Scripts/Door.cs
@@ -1,16 +1,12 @@
-using System;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
     public bool IsOpened { get; private set; }
     
-    public event Action Opened;
-    
     public void Open()
     {
         IsOpened = true;
-        Opened?.Invoke();
     }
     
     public void Close()
